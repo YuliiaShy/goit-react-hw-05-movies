3c7fe688bf4f528de272f2e31b0899175e127d91
Yuliia Shybyk <j7557500@gmail.com>
2022-07-18 23:09:08 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Layout } from './components/Layout/Layout';
 
